f5da86069467a7ddf9d281dcd89ca6dc40e06464
Pavel Ivanov <grammka@gmail.com>
2021-10-26 13:10:56 +0100

    update meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,10 +28,6 @@ const Meta = () => (
     <meta property="twitter:description" content={meta.description} />
     <meta property="twitter:image" content={meta.image} />
     <meta property="twitter:site" content="@ClearpoolFin" />
-    <link id="favicon-180" rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-    <link id="favicon-32" rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-    <link id="favicon-16" rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-    <link rel="manifest" href="/favicon/site.webmanifest" />
   </>
 )
 
