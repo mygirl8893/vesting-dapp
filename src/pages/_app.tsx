8c7ead6b1ade8f7b21cc972f364ef2262513ed3a
Pavel Ivanov <grammka@gmail.com>
2021-10-27 11:40:37 +0100

    add intercom script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,17 +25,30 @@ const SafeHydrate = ({ children }) => (
 const MyApp = ({ Component, pageProps }) => {
 
   return (
-    <SafeHydrate>
-      <Script type="text/javascript" src="/js/css-variables.js" />
-      <Web3ReactProvider getLibrary={getWeb3ReactLibrary}>
-        <ContractsProvider>
-          <Connector>
-            <Component {...pageProps} />
-            <div id="modals" />
-          </Connector>
-        </ContractsProvider>
-      </Web3ReactProvider>
-    </SafeHydrate>
+    <>
+      <SafeHydrate>
+        <Script type="text/javascript" src="/js/css-variables.js" />
+        <Web3ReactProvider getLibrary={getWeb3ReactLibrary}>
+          <ContractsProvider>
+            <Connector>
+              <Component {...pageProps} />
+              <div id="modals" />
+            </Connector>
+          </ContractsProvider>
+        </Web3ReactProvider>
+      </SafeHydrate>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.intercomSettings = {
+              app_id: "bj00s8rr"
+            };
+            
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/bj00s8rr';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+          `
+        }}
+      />
+    </>
   )
 }
 
